0c33d017a5352233491e5710d1b74392d75da4a8
Your Name <you@example.com>
2021-12-15 21:04:55 +0300

    first commit

---
+++ Salwah/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Salwah
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.phone = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(249, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_ClickAsync);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(333, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Authorization";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(230, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(248, 215);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(182, 109);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(249, 330);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Send Message";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(248, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(182, 171);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.name,
+            this.phone});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 38);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(230, 316);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.ListView1_SelectedIndexChanged);
+            // 
+            // name
+            // 
+            this.name.Text = "Full Name";
+            this.name.Width = 115;
+            // 
+            // phone
+            // 
+            this.phone.Text = "Phone";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(407, 353);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(25, 13);
+            this.linkLabel1.TabIndex = 8;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Exit";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(444, 375);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader name;
+        private System.Windows.Forms.ColumnHeader phone;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Salwah/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TeleSharp.TL;
+using TeleSharp.TL.Contacts;
+using TeleSharp.TL.Upload;
+using TLSharp.Core;
+
+namespace Salwah
+{
+    public partial class Form1 : Form
+    {
+
+        int apiId = 11855965; //API ID
+        string apiHash = "45210851de36ff7adb3400cb97beea44"; //TOKEN
+        TelegramClient client;
+        string myPhoneNumber = "+9647505667916"; //PHONE NUMBER
+        string hash;
+        TLContacts tLContact;
+        string userIDSelected;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button1_ClickAsync(object sender, EventArgs e)
+        {
+            File.Delete("session.dat");
+            client = new TelegramClient(apiId, apiHash);
+            await client.ConnectAsync();
+            hash = await client.SendCodeRequestAsync(myPhoneNumber);
+        }
+
+        private async void Button2_Click(object sender, EventArgs e)
+        {
+
+
+            await client.MakeAuthAsync(myPhoneNumber, hash, textBox1.Text);
+            tLContact = await client.GetContactsAsync();
+
+            foreach (TLUser user in tLContact.Users)
+            {
+                if (!string.IsNullOrEmpty(user.Phone))
+                {
+                    var fullName = $"{user.FirstName} {user.LastName}";
+                    var phone = user.Phone;
+                    string[] row = { fullName, phone, user.Id.ToString() };
+                    var listviewItem = new ListViewItem(row);
+                    listView1.Items.Add(listviewItem);
+                }
+
+            }
+
+        }
+
+        //public Image ReadImageFromUser(TLUser user)
+        //{
+        //    //if (user.Photo == null) return Properties.Resources.
+        //    var photo = ((TLUserProfilePhoto)user.Photo);
+        //    var photoLocation = (TLFileLocation)photo.PhotoBig;
+
+        //    TLFile file = client.GetFile(new TLInputFileLocation()
+        //    {
+        //        LocalId = photoLocation.LocalId,
+        //        Secret = photoLocation.Secret,
+        //        VolumeId = photoLocation.VolumeId
+        //    }, 1024 * 256).Result;
+
+        //    Image image;
+        //    using (var m = new MemoryStream(file.Bytes))
+        //    {
+        //        image = Image.FromStream(m);
+        //    }
+        //    return image;
+        //}
+
+        private async void Button3_Click(object sender, EventArgs e)
+        {
+            await client.SendMessageAsync(new TLInputPeerUser() { UserId = Convert.ToInt32(userIDSelected) }, textBox2.Text);
+        }
+
+
+        private void ListView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+                return;
+            ListViewItem item = listView1.SelectedItems[0];
+            var phone = item.SubItems[1].Text;
+            userIDSelected = item.SubItems[2].Text;
+
+            var user = tLContact.Users
+               .Where(x => x.GetType() == typeof(TLUser))
+               .Cast<TLUser>()
+               .FirstOrDefault(x => x.Phone == phone);
+
+            //var userPhoto = ReadImageFromUser(user);
+            //pictureBox1.Image = userPhoto;
+        }
+
+        private void LinkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
